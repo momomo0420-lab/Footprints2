a0b2c127745e9f229cccddd178770e59cdd72a0d
momomo0420-lab <momomo0420.lab+github@gmail.com>
2022-03-12 18:31:53 +0900

    Implemented markers and polylines.

--- app/src/main/java/com/example/footprints2/model/repository/database/MyLocationDatabase.kt
+++ app/src/main/java/com/example/footprints2/model/repository/database/MyLocationDatabase.kt
@@ -2,8 +2,24 @@ package com.example.footprints2.model.repository.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
+import java.sql.Date
 
 @Database(entities = [MyLocation::class], version = 1, exportSchema = false)
+//@TypeConverters(Converters::class)
 abstract class MyLocationDatabase : RoomDatabase() {
     abstract fun myLocationDao(): MyLocationDao
 }
+
+//class Converters {
+//    @TypeConverter
+//    fun fromTimestamp(value: Long?): Date? {
+//        return value?.let { Date(it) }
+//    }
+//
+//    @TypeConverter
+//    fun dateToTimestamp(date: Date?): Long? {
+//        return date?.time
+//    }
+//}
